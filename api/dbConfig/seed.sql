ac667d351f2567e5ae3d72e936cddae38bff8d91
Natalie <58271566+natbibi@users.noreply.github.com>
2021-03-31 14:14:38 +0100

    Revert "Coding challenge"

--- api/dbConfig/seed.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO posts (title,name,body)
-VALUES
-(
-  "Environment",
-  "Adil",
-  "Let's save the Environment"
-)
--- api/dbConfig/setup.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS posts;
-
-CREATE TABLE posts (
-  id serial PRIMARY KEY,
-  title varchar(120) NOT NULL,
-  name varchar(30) NOT NULL,
-  body varchar(255) NOT NULL,
-);
